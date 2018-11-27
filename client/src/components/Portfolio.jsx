1b0c4189c4dbaa3a7e0fbb5fb8b6b91e949e3f2e
alin684 <al684@cornell.edu>
2018-11-27 17:00:47 -0500

    buy stock component

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -9,10 +9,11 @@ class Portfolio extends Component {
       money: '',
       ticker: '',
       shares: '',
+      transactions: '',
     }
     this.handleChange = this.handleChange.bind(this);
     this.makeTransaction = this.makeTransaction.bind(this);
-    // this.getTransactions = this.getTransactions.bind(this);
+    this.getTransactions = this.getTransactions.bind(this);
   }
 
   componentDidMount(){
@@ -40,6 +41,65 @@ class Portfolio extends Component {
   }
 
   makeTransaction() {
+    let money = this.state.money;
+    let ticker = this.state.ticker;
+    let shares = this.state.shares;
+
+    console.log('what')
+
+    fetch(`https://api.iextrading.com/1.0/stock/${ticker}/price`)
+      .then(res => {
+        if (res.status == 200) {
+          return res.json();
+        } else {
+          throw new Error("Something went wrong")
+        }
+      })
+      .then(price => {
+        if ((price * shares) < money) {
+          fetch('/makeTransaction', {
+            method: 'POST',
+            body: JSON.stringify({ ticker, price, shares }),
+            headers: {
+              'Content-Type': 'application/json',
+            }
+          }).then(res => res.json())
+          .then(res => {
+            if (res) {
+              this.getTransactions();
+            } else {
+              throw new Error("Not enough money");
+            }
+          })
+        } else {
+          throw new Error("Not enough money");
+        }
+      })
+  }
+
+  getTransactions() {
+    fetch('/getTransactions', {
+      method: "GET",
+      headers: {
+        token: Auth.getToken(),
+        'Authorization': `Token ${Auth.getToken()}`,
+      }
+    }).then(res => {
+      return res.json();
+    }).then(res => {
+      let money = res.money;
+      let transactions = res.transactions.map(trans => {
+        return (
+          <p>
+            {`You have ${trans.shares} shares of ${trans.ticker} stock.`}
+          </p>
+        )
+      })
+      this.setState({
+        transactions: transactions,
+        money: money
+      })
+    });
 
   }
 
