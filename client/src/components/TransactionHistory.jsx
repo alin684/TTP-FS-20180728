16b7eb553c3e2729790962c4b48cdafc03e3890a
alin684 <al684@cornell.edu>
2018-12-25 20:48:19 -0500

    revert

--- client/src/components/TransactionHistory.jsx
+++ client/src/components/TransactionHistory.jsx
@@ -62,19 +62,6 @@ class TransactionHistory extends Component {
     }
   }
 
-  showTransactions() {
-    if (this.state.transactions) {
-      return this.state.transactions.map(trans => {
-        return (
-          <div key={trans.id}>
-            Transactions
-            {trans.ticker.toUpperCase()} stock: {trans.shares} shares
-          </div>
-        )
-      })
-    }
-  }
-
   render() {
     return (
       <div>
