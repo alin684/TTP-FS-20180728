214a277ed9ddba10d872e6e479fff98b66272060
alin684 <al684@cornell.edu>
2019-01-01 22:08:57 -0500

    get

--- client/src/components/SwapHistory.jsx
+++ client/src/components/SwapHistory.jsx
@@ -15,10 +15,42 @@ class TransactionHistory extends Component {
     this.showTransactions = this.showTransactions.bind(this);
   }
 
+}
+this.handleChange = this.handleChange.bind(this);
+this.getTransactions = this.getTransactions.bind(this);
+this.showTransactions = this.showTransactions.bind(this);
+}
+
 handleChange(e) {
 const name = e.target.name;
 const value = e.target.value;
 this.setState({
   [name]: value,
 });
+}
+
+componentDidMount(){
+this.getTransactions();
+}
+
+getTransactions() {
+fetch('/getTransactions', {
+  method: "GET",
+  headers: {
+    token: Auth.getToken(),
+    'Authorization': `Token ${Auth.getToken()}`,
+  }
+}).then(res => {
+  return res.json();
+}).then(res => {
+  this.setState({
+    money: res.money,
+    transactions: res.transactions,
+    stocks: res.stocks,
+  })
+  console.log(this.state.money)
+  console.log(this.state.transactions)
+  console.log(this.state.stocks)
+});
+
 }
