214c6c6225f3c23a8bcca74301f661dba5c88ee2
alin684 <al684@cornell.edu>
2019-01-07 23:35:22 -0500

    test2

--- client/src/components/SwapHistory.jsx
+++ client/src/components/SwapHistory.jsx
@@ -84,13 +84,7 @@ showTransactions() {
 if (this.state.transactions) {
   return this.state.transactions.map(trans => {
     return (
-      <div key={trans.id}>
-        Transactions
-        {trans.ticker.toUpperCase()} stock: {trans.shares} shares
-      </div>
-    )
-  })
-}
+
 
 
 export default TransactionHistory;
