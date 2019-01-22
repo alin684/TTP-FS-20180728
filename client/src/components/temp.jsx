da9f4680d3958035d91d3324763eee5c52e6991e
alin684 <al684@cornell.edu>
2019-01-22 12:43:14 -0500

    menu test

--- client/src/components/temp.jsx
+++ client/src/components/temp.jsx
@@ -8,8 +8,12 @@ let transactions = res.transactions.map(trans => {
   console.log(trans)
 })
 
-render() {
-  return (
-    <div>
-      {this.showTransactions()}
-    </div>
+<Menu className="navBar" inverted fixed="bottom">
+  <Menu.Menu position='right'>
+  <Menu.Item name='login' as={Link} to='/login' />
+  <Menu.Item name='register' as={Link} to='/register' />
+  <Menu.Item name='portfolio' as={Link} to='/portfolio' />
+  <Menu.Item name='transactionHistory' as={Link} to='/transactionHistory' />
+  <Menu.Item name='logout' onClick={this.handleLogout} />
+  </Menu.Menu>
+</Menu>
