199d1755fb69624ba5d7fa36b4b24fa68369c337
alin684 <al684@cornell.edu>
2019-02-04 15:22:56 -0500

    menu update

--- client/src/components/temp.jsx
+++ client/src/components/temp.jsx
@@ -14,9 +14,11 @@ end
   <Menu.Item name='login' as={Link} to='/login' />
   <Menu.Item name='register' as={Link} to='/register' />
   <Menu.Item name='transactionHistory' as={Link} to='/transactionHistory' />
-  <Menu.Item name='logout' onClick={this.handleLogout} />
+  <Menu.Item name='log' onClick={this.handleLogout} />
   <Menu.Item name='register' as={Link} to='/register' />
   <Menu.Item name='transactionHistory' as={Link} to='/transactionHistory' />
-  
+  <Menu.Item name='button' as={Link} to='/register' />
+  <Menu.Item name='transactionHistory' as={Link} to='/transactionHistory' />
+
   </Menu.Menu>
 </Menu>
