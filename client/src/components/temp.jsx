3eaed247cd31a3c320712f73604ed6ee29951e08
alin684 <al684@cornell.edu>
2019-02-04 15:22:05 -0500

    menu url

--- client/src/components/temp.jsx
+++ client/src/components/temp.jsx
@@ -13,7 +13,6 @@ end
   <Menu.Menu position='right'>
   <Menu.Item name='login' as={Link} to='/login' />
   <Menu.Item name='register' as={Link} to='/register' />
-  <Menu.Item name='portfolio' as={Link} to='/portfolio' />
   <Menu.Item name='transactionHistory' as={Link} to='/transactionHistory' />
   <Menu.Item name='logout' onClick={this.handleLogout} />
   </Menu.Menu>
