8df49733f7f2407cca366d30218d59a9b8cceef7
alin684 <al684@cornell.edu>
2019-02-04 15:21:39 -0500

    menu correction

--- client/src/components/temp.jsx
+++ client/src/components/temp.jsx
@@ -8,3 +8,13 @@ let transactions = res.transactions.map(trans => {
 )
 
 end
+
+<Menu className="navBar" inverted fixed="bottom">
+  <Menu.Menu position='right'>
+  <Menu.Item name='login' as={Link} to='/login' />
+  <Menu.Item name='register' as={Link} to='/register' />
+  <Menu.Item name='portfolio' as={Link} to='/portfolio' />
+  <Menu.Item name='transactionHistory' as={Link} to='/transactionHistory' />
+  <Menu.Item name='logout' onClick={this.handleLogout} />
+  </Menu.Menu>
+</Menu>
